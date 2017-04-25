1a6c40d7e660640453f9155d75c7bb106cc218a7
Michael Le <mjle@ucsc.edu>
2017-04-24 22:18:21 -0700

    first commit

---
+++ ProgressBarFun/ProgressBarView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressBarView.swift
+//  ProgressBarFun
+//
+//  Created by Michael Le on 4/18/17.
+//  Copyright © 2017 Michael Le. All rights reserved.
+//
+
+import UIKit
+
+class ProgressBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
